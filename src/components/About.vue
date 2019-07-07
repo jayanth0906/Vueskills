c07859f132cff8e27a2f63145758a632fefbf0cd
jayanth0906 <42557399+jayanth0906@users.noreply.github.com>
2019-07-06 20:17:32 -0500

    Intial commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+        <h1>Hello {{ their_name }}, this is Jayanth's about page</h1>
+    <p>This is Jaynth’s To Do list developed in Vue.Js to learn Vue.Js</p>
+  </div>
+</template>
+
+<script>
+        export default {
+          name: 'About',
+          data() {
+            return {
+              their_name: this.$route.params.name
+            }
+          }
+        }
+        </script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VeeValidate from 'vee-validate';  // Add this
+import router from './router'
+
+Vue.use(VeeValidate);  // Add this
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
